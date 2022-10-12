bea84d720c5e2e960f13bbf0fcbcbf1e4e728baf
Euler <mombergereuler@gmail.com>
2022-10-12 12:09:15 -0300

    feat: Data fetching e cache local

---
+++ src/services/hooks/useUsers.ts
@@ -0,0 +1,42 @@
+import { useQuery } from 'react-query';
+import { api } from '../api';
+
+type User = {
+  id: string;
+  name: string;
+  email: string;
+  created_at: string;
+};
+
+type GetUsersResponse = {
+  totalCount: number;
+  users: User[];
+};
+
+export const getUsers = async (page: number): Promise<GetUsersResponse> => {
+  const { data, headers } = await api.get('/users', { params: { page } });
+
+  const totalCount = Number(headers['x-total-count']);
+
+  const users = data.users.map((user: User) => ({
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    created_at: new Date(user.created_at).toLocaleDateString('pt-BR', {
+      day: '2-digit',
+      month: 'long',
+      year: 'numeric'
+    })
+  }));
+
+  return {
+    users,
+    totalCount
+  };
+};
+
+export function useUsers(page: number) {
+  return useQuery(['users', page], () => getUsers(page), {
+    staleTime: 1000 * 60 * 10
+  });
+}
